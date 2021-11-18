769f772350b18045864d6cf35a9771c8d15e909d
margo <margoshka.ri6@gmail.com>
2021-11-18 13:32:31 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormClosing += Form1_FormClosing;
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.bookList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(385, 467);
+            this.textBox1.TabIndex = 0;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(78, 506);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 1;
+            this.addBtn.Text = "add book";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(159, 506);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(75, 23);
+            this.playBtn.TabIndex = 2;
+            this.playBtn.Text = "play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Location = new System.Drawing.Point(240, 506);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(75, 23);
+            this.pauseBtn.TabIndex = 2;
+            this.pauseBtn.Text = "pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // bookList
+            // 
+            this.bookList.FormattingEnabled = true;
+            this.bookList.ItemHeight = 15;
+            this.bookList.Location = new System.Drawing.Point(403, 12);
+            this.bookList.Name = "bookList";
+            this.bookList.ScrollAlwaysVisible = true;
+            this.bookList.Size = new System.Drawing.Size(163, 529);
+            this.bookList.TabIndex = 3;
+            this.bookList.SelectedIndexChanged += new System.EventHandler(this.bookList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 550);
+            this.Controls.Add(this.bookList);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.ListBox bookList;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        List<string> fullPath = new List<string>();
+        bool isPaused = false;
+        Task task;
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists("books.txt"))
+            {
+                fullPath = File.ReadAllLines("books.txt").ToList();
+                fullPath.ForEach(item =>
+                {
+                    if (File.Exists(item))
+                        this.bookList.Items.Add(Path.GetFileName(item));
+                });
+            }
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                openFile.Filter = "txt files (*.txt)|*.txt";
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    fullPath.Add(openFile.FileName);
+                    this.bookList.Items.Add(Path.GetFileName(openFile.FileName));
+                }
+            }
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            if (!isPaused)
+            {
+                task = new Task(() =>
+                {
+                    synthesizer.Volume = 100;  // 0...100
+                    synthesizer.Rate = -2;     // -10...10// Synchronous
+                    synthesizer.Speak(this.textBox1.Text);
+
+                });
+                task.Start();
+            }
+            else
+                synthesizer.Resume();
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            synthesizer.Pause();
+            isPaused = true;
+        }
+
+        private void bookList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if ((sender as ListBox).SelectedItem != null)
+            {
+                int index = fullPath.FindIndex(item => item.Contains((sender as ListBox).SelectedItem.ToString()));
+                this.textBox1.Text = File.ReadAllText(fullPath[index]);
+                isPaused = false;
+                synthesizer.Pause();
+                synthesizer = new SpeechSynthesizer();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            File.WriteAllLines("books.txt", fullPath);
+        }
+    }
+}
